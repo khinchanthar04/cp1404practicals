14ef4ad27361751665a76d6011677cfafb6375d5
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-06 14:44:08 +0800

    password_stars

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,18 @@
 PASSWORD_LENGTH = 10
 
 def main():
+    """"Get and print password using functions."""
     user_password = get_password()
     print_asterisks(user_password)
 
 
 def print_asterisks(user_password):
+    """Print asterisks as the length of characters in password."""
     print("*" * user_password)
 
 
 def get_password():
+    """Get password meeting the required characters in PASSWORD_LENGTH."""
     user_password = len(input("Enter your password: "))
     while user_password <= PASSWORD_LENGTH:
         print("Invalid Password")
