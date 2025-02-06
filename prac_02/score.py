866ba6df1d0a84668094e3898c798a2c68ad9246
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-06 16:28:03 +0800

    Refactor score program to use functions

---
+++ prac_02/score.py
@@ -0,0 +1,27 @@
+import random
+
+def main():
+    """Get user score and random score then display result for both."""
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+    random_score = random.randint(0,100)
+    print(f"Random score : {random_score}")
+    random_result = determine_result(random_score)
+    print(random_result)
+
+
+def determine_result(score):
+    """Determine the result based on the given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
