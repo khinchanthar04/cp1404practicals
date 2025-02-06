4376912a46e1e7589ab07548d046ab41d1e87bd0
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-06 18:10:37 +0800

    Refactor score program with menu to using functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,54 @@
+MENU = """(G)et a valid score
+(P)rint the result
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    score = ""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+            print(f"Your score is : {score}")
+        elif choice == "P":
+            if score == "":
+                print("Please enter a score!")
+            else:
+                result = determine_result(score)
+                print(f"Your Result is : {result}")
+        elif choice == "S":
+            if score == "":
+                print("Please enter a score!")
+            else:
+                print_asterisks(int(score))
+        else:
+            print("Invalid Choice!")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def get_valid_score():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score please enter again!")
+        score = float(input("Enter score: "))
+    return score
+
+
+def determine_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def print_asterisks(score):
+    """Print as many asterisks as the score."""
+    print("* " * score)
+
+
+main()
