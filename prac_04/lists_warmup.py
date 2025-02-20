7b455a0f5407681c7eb74f661aaa147ed91b3917
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-20 16:09:13 +0800

    adding finalized version of lists warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,14 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# what values do the following expressions have?
+# The following expression have a list containing integers as elements.
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+numbers[-1] = 1
+
+# print all the elements from numbers except the first two
+print(numbers[2:])
+
+# print whether 9 in an element of numbers
+print(9 in numbers)
