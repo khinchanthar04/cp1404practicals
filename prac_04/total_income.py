c15bb0199ca1f293a5dfe769b98f3babbccf50e1
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-23 14:38:08 +0800

    Updated the strings into f string

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 Practical
-Starter code for cumulative total income program
+Calculating a List of Cumulative Totals
 """
 
 
@@ -10,7 +10,7 @@ def main():
     months = int(input("How many months? "))
 
     for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
@@ -18,7 +18,7 @@ def main():
     for month in range(1, months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
