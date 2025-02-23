9544861351e575e3588dfe4f203fdc02d9ef8e47
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-23 15:16:42 +0800

    Refactored code: Moved print income report into a separate function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -20,7 +20,6 @@ def print_income_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, 1):
-        income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
