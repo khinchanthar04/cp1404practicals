dcfd6f0cdb821c7604fe9fe1b08ae65b2c186e10
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-23 14:48:48 +0800

    Refactored code: Moved print income report into a separate function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,21 +1,25 @@
 """
 CP1404/CP5632 Practical
-Calculating a List of Cumulative Totals
+Program for calculating a List of Cumulative Totals
 """
 
 
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_income_report(incomes)
+
+
+def print_income_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month, income in enumerate(incomes, 1):
         income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
