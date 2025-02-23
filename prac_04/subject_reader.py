c53316a2547406b484b0b4fb88c04d5fdd87d8c3
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-23 16:04:40 +0800

    Refactoring subject data processing by adding display_subjects function and load_subjects now returns a nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,32 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    """Read subject data and display neatly."""
+    subjects = load_subjects()
+    display_subjects(subjects)
 
 
-def load_data():
+def load_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        subject.append(parts)
     input_file.close()
+    return subject
+
+
+def display_subjects(subjects):
+    """Display subject details in a neat manner."""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 main()
