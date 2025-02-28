92f5e23aa504a8bfedce98c64cc1f1d9161fd025
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-28 15:34:13 +0800

    Print the dictionary in a formatted manner

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -13,11 +13,14 @@ CODE_TO_NAME = {"QLD": "Queensland",
                 "VIC": "Victoria",
                 "TAS": "Tasmania",
                 "SA": "South Australia"}
-print(CODE_TO_NAME)
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
 state = input("Enter short state: ").upper()
 while state != "":
     if state in CODE_TO_NAME:
-        print(f"{state} is {CODE_TO_NAME[state]}")
+        print(f"{state:3} is {CODE_TO_NAME[state]}")
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
