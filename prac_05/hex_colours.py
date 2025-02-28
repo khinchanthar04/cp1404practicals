d27bdd5c256ca46c03d07511c6dae47d8ead0ad6
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-02-28 16:29:46 +0800

    Initial commit for hexadecimal colour lookup programme

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,24 @@
+"""
+CP1404 Practical 5
+Hexadecimal colour lookup
+"""
+
+COLOUR_CODES = {"aliceblue": "#f0f8ff",
+                "byzantium": "#702963",
+                "cadetgrey": 	"#91a3b0",
+                "cadmiumgreen": "#006b3c",
+                "cadmiumorange": "#ed872d",
+                "candyapplered": "#ff0800",
+                "electricblue": "#7df9ff",
+                "mindaro": "#e3f988"}
+
+
+colour_name = input("Enter a colour name: ")
+
+while colour_name != "":
+    hex_code = COLOUR_CODES.get(colour_name)
+    if hex_code:
+        print(f"The code for \"{colour_name}\" is {COLOUR_CODES.get(colour_name)}")
+    print("Invalid colour, please try again.")
+    colour_name = input("Enter a colour name: ")
+
