2e8de2a095176cad5625743276069c4899f0fbc1
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-01 22:27:35 +0800

    Refactor word occurrence counter to improve by improving handling, sorting and alignment

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,23 +4,19 @@ Estimate: 20 minutes
 Actual:   37 minutes
 """
 
-user_string = input("Please enter text: ")
-words = user_string.lower().split()
-word_counts = {}
+text = input("Please enter text: ")
+words = text.lower().split()
+word_to_count = {}
 
 for word in words:
     word = word.strip('.,!?;:"\'')
-    if word in word_counts:
-        word_counts[word] += 1
-    else:
-        word_counts[word] = 1
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
+print(f"Text: {text}")
 
-sorted_word_counts = dict(sorted(word_counts.items()))
-max_word_length = max(len(word) for word in sorted_word_counts.keys())
+unique_words = list(word_to_count.keys())
+unique_words.sort()
+max_word_length = max(len(word) for word in unique_words)
 
-# Print the input string
-print(f"Text: {user_string}")
-
-# Print the word counts in a sorted and aligned format
-for word, count in sorted_word_counts.items():
-    print(f"{word:{max_word_length}} : {count}")
+for word in unique_words:
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
