bf298ac7af798ff9e38ef797eac81d09419a8a85
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-11 09:52:21 +0800

    Add string representation function and name

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,17 +4,22 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
         self.fuel += amount
+
+    def __str__(self):
+        """Return a string for a Car object."""
+        return f"{self.name}, fuel = {self.fuel}, odometer = {self._odometer}"
 
     def drive(self, distance):
         """Drive the car a given distance.
