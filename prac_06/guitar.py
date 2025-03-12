3a1e59a3eb0b70d3ad080271dfdf2f95a2638fc9
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-12 18:11:32 +0800

    Add docstrings for

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,13 +4,17 @@ Guitar class
 """
 
 class Guitar:
+    """Represents a guitar with a name, year of manufacture, and cost."""
     def __init__(self, name, year, cost):
+        """Initialize a Guitar instance with name, year, and cost."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def get_age(self):
+        """Return the age of the guitar based on the current year."""
         return 2022 - self.year  # Change to current year if needed
 
     def is_vintage(self):
+        """Determine if a guitar is considered vintage or not based on age."""
         return self.get_age() >= 50
