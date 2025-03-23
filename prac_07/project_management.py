d459801e95e5031d07b1e9bac0b173aad857f751
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-23 19:14:56 +0800

    Refactor load project to read the txt file and return a list of projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,6 +1,23 @@
 """
 CP1404 Practical - 7
-Estimated time : 3 hours
+Estimated time : 2 hours
 Actual time:
 Project Management
 """
+from prac_07.project import Project
+
+def main():
+    projects = load_projects(filename="projects.txt")
+
+
+def load_projects(filename="projects.txt"):
+    """Load projects from the txt file and return a list of project objects."""
+    projects = []
+    try:
+        with open(filename, "r") as file:
+            next(file)
+            for line in file:
+                name, start_date, priority, cost_estimate, completion_percentage = line.strip().split('\t')
+                projects.append(Project(name,start_date, priority, cost_estimate, completion_percentage))
+        return projects
+
