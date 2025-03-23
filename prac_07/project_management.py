90ed86585e090b98c0c6387aae5d5d24f531defa
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-23 19:46:06 +0800

    Implement main program structure and core function like display projects, save projects and load project for project management system

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,20 +4,67 @@ Estimated time : 2 hours
 Actual time:
 Project Management
 """
+
 from prac_07.project import Project
+from datetime import datetime
+
+FILENAME = "projects.txt"
+MENU = ("- (L)oad projects \n"
+        "- (S)ave projects \n"
+        "- (D)isplay projects \n"
+        "- (F)ilter projects by date) \n"
+        "- (A)dd new project\n"
+        "- (U)pdate project\n"
+        "- (Q)uit")
 
 def main():
-    projects = load_projects(filename="projects.txt")
+    """Program for a project management that track project completion and project details that user wish to complete."""
+    projects = load_projects(FILENAME)
+    print("Welcome to Pythonic Project Management")
+    print(f"Load {len(projects)} projects from {FILENAME}")
+    print(MENU)
+
+    choice =input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+           filename = input("Filename: ")
+           projects = load_projects(filename)
+        elif choice == "S":
+            filename = input("Filename: ")
+            save_projects(filename, projects)
+        elif choice == "D":
+            display_projects(projects)
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
 
 
-def load_projects(filename="projects.txt"):
+def load_projects(filename):
     """Load projects from the txt file and return a list of project objects."""
     projects = []
-    try:
-        with open(filename, "r") as file:
-            next(file)
-            for line in file:
-                name, start_date, priority, cost_estimate, completion_percentage = line.strip().split('\t')
-                projects.append(Project(name,start_date, priority, cost_estimate, completion_percentage))
-        return projects
+    with open(filename, "r") as file:
+        file.readline() #Skip header line
+        for line in file:
+            name, start_date, priority, cost_estimate, completion_percentage = line.strip().split('\t')
+            projects.append(Project(name,start_date, priority, cost_estimate, completion_percentage))
+    return projects
 
+def save_projects(filename, projects):
+    """Save the list of projects to the file."""
+    with open(filename, "w", newline="") as file:
+        file.readline()
+        for project in projects:
+            file.write(f"{project.name},{project.start_date},{project.priority},{project.cost_estimate},{project.completion_percentage}\n")
+
+def display_projects(projects):
+    """Display all projects."""
+    incomplete = [project for project in projects if not project.is_complete()]
+    complete = [project for project in projects if project.is_complete()]
+    print("Incomplete projects: ")
+    for project in sorted(incomplete):
+        print(f"  {project}")
+
+    print("Complete Projects: ")
+    for project in sorted(complete):
+        print(f"  {project}")
