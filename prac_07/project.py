f0f46828fd8022cdbb2c92eb743207340db1f10b
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-03-23 19:16:18 +0800

    Implement Project class with initialization, string representation, comparison, and update methods

--- prac_07/project.py
+++ prac_07/project.py
@@ -7,10 +7,28 @@ Project
 import datetime
 
 class Project:
+    """
+    A class to represent a project.
+    """
     def __init__(self,name,start_date,priority, cost_estimate, completion_percentage):
+        """Initialize a project object with given attributes."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
 
+    def __str__(self):
+        """Return a string representation of the project."""
+        return (f"{self.name}, start: {self.start_date.strftime("%d/%m/%Y")}, priority:{self.priority}, "
+                f"cost: {self.cost_estimate:.2f}. completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        """Define less than operator for sorting by priority."""
+        return self.priority < other.priority
+
+    def update(self, new_completion_percentage = None, new_priority = None):
+        if new_completion_percentage is not None:
+            self.completion_percentage = int(new_completion_percentage)
+        if new_priority is not None:
+            self.priority = int(new_priority)
