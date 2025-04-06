f6aa609d67b588df5cf37fd6c9157c948133a8d8
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-04-06 18:06:28 +0800

    Add driving functionality and fare calculation to Taxi Simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,7 +3,6 @@ CP1404 Practical - 9
 Taxi Simulator Program
 """
 
-from prac_09.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
@@ -18,9 +17,9 @@ def main():
              SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MENU)
-    menu_choice = input (">>> ").upper()
-    while menu_choice != "Q":
-        if menu_choice == "C":
+    menu_choice = input (">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
             print("Available Taxis: ")
             display_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
@@ -33,21 +32,32 @@ def main():
                     print("Invalid taxi choice")
             except ValueError and IndexError:
                 print("Invalid Input!")
-        elif menu_choice == "D":
-            print("Drive")
+        elif menu_choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid Option")
-        print("Bill to date: $ . . . ")
+        print(f"Bill to date: $ {total_bill:.2f} ")
         print(MENU)
-        menu_choice = input(">>> ").upper()
+        menu_choice = input(">>> ").lower()
 
-    print(f"Total trip cost:")
+    print(f"Total trip cost: ${total_bill:.2f}")
     print("Taxis are now: ")
+    display_taxis(taxis)
 
 def display_taxis(taxis):
     """Display all available taxis."""
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
 
 
 if __name__ == '__main__':
