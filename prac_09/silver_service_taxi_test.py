bb02ed3799e4c06a8da8d18c6a99e5e6d9fa980e
KhinChanThar <khinchan.thar@my.jcu.edu.au>
2025-04-06 15:37:21 +0800

    Add test for SilverServiceTaxi fare calculation with assert using float tolerance

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,3 +3,19 @@ CP1404 Practical - 9
 SilverServiceTaxi class Tests
 
 """
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Test for SilverServiceTaxi class."""
+
+    hummer = SilverServiceTaxi("Hummer", 200, 2)
+    hummer.start_fare()
+    hummer.drive(18)
+    print(hummer)
+    fare = hummer.get_fare()
+    print(f"Fare: ${fare}")
+
+    assert abs(fare - 48.78)< 0.01, f"Expected $48.80 but got ${fare:.2f}"
+if __name__ == '__main__':
+    main()
